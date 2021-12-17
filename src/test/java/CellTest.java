513c04de744551a4bf39e5f2ebcb8072c43a5ae3
Adam Jonas <adamjonas25@gmail.com>
2021-12-17 23:03:59 +0000

    Enable Cell to be revived

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,15 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
+    private Cell cell;
+
+    @BeforeEach
+    void setUp() {
+        cell = new Cell();
+    }
 
     @Test
     void kill() {
-        Cell cell = new Cell();
         assertTrue(cell.isAlive());
 
         cell.kill();
         assertFalse(cell.isAlive());
+    }
+
+    @Test
+    void revive() {
+        cell.kill();
+        assertFalse(cell.isAlive());
+
+        cell.revive();
+        assertTrue(cell.isAlive());
     }
 }
