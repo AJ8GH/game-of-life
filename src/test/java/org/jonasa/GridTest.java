a01ea339529f4102c23dee18f71e85ed8d301431
Adam Jonas <adamjonas25@gmail.com>
2021-12-19 16:20:31 +0000

    Wrap grid

--- src/test/java/org/jonasa/GridTest.java
+++ src/test/java/org/jonasa/GridTest.java
@@ -53,4 +53,45 @@ class GridTest {
         verify(liveCell, times(4)).setNeighbours(5);
         verify(liveCell, times(1)).setNeighbours(8);
     }
+
+    @Test
+    void tick_GridWrapsCorrectly() {
+        Cell liveCell = mock(Cell.class, "liveCell");
+        when(liveCell.isAlive()).thenReturn(true);
+        when(liveCell.toString()).thenReturn("1");
+
+        Cell deadCell = mock(Cell.class, "deadCell");
+        when(deadCell.isAlive()).thenReturn(false);
+        when(deadCell.toString()).thenReturn("0");
+
+        Cell topLeft = mock(Cell.class, "topLeft");
+        when(topLeft.isAlive()).thenReturn(true);
+        when(topLeft.toString()).thenReturn("1");
+
+        Cell topRight = mock(Cell.class, "topRight");
+        when(topRight.isAlive()).thenReturn(true);
+        when(topRight.toString()).thenReturn("1");
+
+        Cell bottomLeft = mock(Cell.class, "bottomLeft");
+        when(bottomLeft.isAlive()).thenReturn(true);
+        when(bottomLeft.toString()).thenReturn("1");
+
+        Cell bottomRight = mock(Cell.class, "bottomRight");
+        when(bottomRight.isAlive()).thenReturn(true);
+        when(bottomRight.toString()).thenReturn("1");
+
+        List<Cell> topRow = List.of(topLeft, liveCell, deadCell, deadCell, topRight);
+        List<Cell> row2 = List.of(liveCell, deadCell, deadCell, deadCell, deadCell);
+        List<Cell> deadRow = List.of(deadCell, deadCell, deadCell, deadCell, deadCell);
+        List<Cell> bottomRow = List.of(bottomLeft, deadCell, deadCell, deadCell, bottomRight);
+        List<List<Cell>> cells = List.of(topRow, row2, deadRow, deadRow, bottomRow);
+
+        Grid victim = new Grid(cells);
+        victim.tick();
+
+        verify(topLeft, times(1)).setNeighbours(5);
+        verify(bottomRight, times(1)).setNeighbours(3);
+        verify(topRight, times(1)).setNeighbours(4);
+        verify(bottomLeft, times(1)).setNeighbours(4);
+    }
 }
