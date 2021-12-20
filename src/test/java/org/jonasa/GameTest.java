b097bc94563099312be2bdaf28a64e6ac2fd6125
Adam Jonas <adamjonas25@gmail.com>
2021-12-20 11:17:11 +0000

    Fix Game test

--- src/test/java/org/jonasa/GameTest.java
+++ src/test/java/org/jonasa/GameTest.java
@@ -17,16 +17,16 @@ public class GameTest {
     }
 
     @Test
-    void run_StopsAfterTwoIdenticalSnapshots() {
+    void run_WhenPopulationIsZero_GameStops() {
         Seeder seeder = mock(Seeder.class);
         Grid grid = mock(Grid.class);
 
         when(grid.population())
-                .thenReturn( 10L)
-                .thenReturn(15L)
-                .thenReturn(8L)
-                .thenReturn(3L)
-                .thenReturn(0L);
+                .thenReturn(10L).thenReturn(10L)
+                .thenReturn(15L).thenReturn(15L)
+                .thenReturn(8L).thenReturn(8L)
+                .thenReturn(3L).thenReturn(3L)
+                .thenReturn(0L).thenReturn(0L);
 
         Game game = new Game(grid, seeder);
         game.run();
